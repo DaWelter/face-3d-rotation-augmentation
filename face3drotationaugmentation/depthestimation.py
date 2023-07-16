a5edc1d4f746190bc3717c4d1580988b7363931e
Michael Welter <michael@welter-4d.de>
2023-07-16 17:54:19 +0200

    Put commit hash in depth estimator download from torch.hub.log

--- face3drotationaugmentation/depthestimation.py
+++ face3drotationaugmentation/depthestimation.py
@@ -14,13 +14,16 @@ def init():
     #model_type = "DPT_Hybrid"   # MiDaS v3 - Hybrid    (medium accuracy, medium inference speed)
     model_type = "MiDaS_small"  # MiDaS v2.1 - Small   (lowest accuracy, highest inference speed)
 
-    midas = torch.hub.load("intel-isl/MiDaS", model_type)
+    # This repo looks legit. I put in the current commit hash. So not much could go wrong here.
+    repository = "intel-isl/MiDaS:1645b7e1675301fdfac03640738fe5a6531e17d6"
+
+    midas = torch.hub.load(repository, model_type, trust_repo=True)
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     midas.to(device)
     midas.eval()
 
-    midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+    midas_transforms = torch.hub.load(repository, "transforms", trust_repo=True)
     if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
         transform = midas_transforms.dpt_transform
     else:
